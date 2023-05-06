b498b2f986a003b57c8ae5dc34532530afb270ac
Lewis Jackson <lewis.jackson10@hotmail.co.uk>
2023-05-06 17:27:42 +0100

    website has suddenly gone offline

--- djangoapi/airline/views.py
+++ djangoapi/airline/views.py
@@ -6,7 +6,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 import csv
-import os
 
 locToId = {}
 with open('countries.csv', mode='r') as csvfile:
