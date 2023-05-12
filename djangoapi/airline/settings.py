1f0023362c524a25dd744ff78fdb6d5ae06fe589
Lewis Jackson <lewis.jackson10@hotmail.co.uk>
2023-05-12 18:18:18 +0100

    new host allowed

--- djangoapi/airline/settings.py
+++ djangoapi/airline/settings.py
@@ -113,6 +113,8 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = "/home/ed19l2j/airline/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
